87a1f3047fe102d7c21c550cdf18924a28362dbd
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2019-10-04 15:40:12 +0200

    feat: implement watch mode for the vue project build
    This replaces normal build for the vue project. This requires the
    removal of vuetify-loader from the vue project. The vuetify-loader
    is used to only bundle components that are used, which is
    unnecessary, because the extension will supply all of vuetify.

--- command_line.py
+++ command_line.py
@@ -1,9 +1,8 @@
 import argparse
-from subprocess import CalledProcessError
 
 from config import (extension_dir, vue_project_dir, settings, ipy_extension_name,
-                    jupyter_extension_name, component_library_used)
-from helpers import copy_directory_recursive, delete_directory, directory_exists, execute
+                    jupyter_extension_name)
+from helpers import delete_directory, directory_exists, execute
 from generate_extension import generate_extension
 
 
@@ -13,9 +12,8 @@ def handle_command_line():
     commands:
         install-dev             Generates jupyter-extension project, builds it and installs in
                                 developement mode (notebook and lab). This has to be done only once.
-        build-lib               Builds vue-cli project with target lib. Use this when code has
-                                changed.
-        watch                   Runs JavaScript build in watch mode.
+        watch-vue-project       Build and watch vue-cli project.
+        watch-extension         Build and watch extension.
         regenerate-project      Regenerated project. Use this when ipyvuelink.yaml has changed
         publish-pypi            Publish extension on PyPi.
         publish-npm             Publish extension on NPM.
@@ -27,9 +25,9 @@ def handle_command_line():
 
     if args.command == 'install-dev':
         dev_install_extension()
-    elif args.command == 'build-lib':
-        build_vue_project()
-    elif args.command == 'watch':
+    elif args.command == 'watch-vue-project':
+        build_vue_project(watch=True)
+    elif args.command == 'watch-extension':
         watch_extension()
     elif args.command == 'regenerate-project':
         regenerate_extension()
@@ -55,13 +53,22 @@ def dev_install_extension():
     execute(None, f'jupyter labextension install {extension_dir}/js --no-build')
 
 
-def build_vue_project():
-    temporary_remove_vuetify_loader_and_execute(
-        _build_vue_project)
+def build_vue_project(watch=False):
+    if not directory_exists(f'{vue_project_dir}/node_modules'):
+        execute(vue_project_dir, 'yarn install')
 
-    copy_directory_recursive(
-        f'{vue_project_dir}/dist',
-        f'{extension_dir}/js/vue_project_dist_mirror')
+    try:
+        # keep using --mode production with --watch, else css is not included
+        execute(
+            vue_project_dir,
+            f'npx vue-cli-service build --target lib \
+            --formats {"umd"} \
+            --dest {extension_dir}/js/vue_project_dist_mirror \
+            --name component src/{settings["entry"]} \
+            --mode production \
+            {"--watch" if watch else ""}')
+    except KeyboardInterrupt:
+        pass
 
 
 def watch_extension():
@@ -89,33 +96,6 @@ def publish_npm():
 
 
 # helpers #
-
-
-def temporary_remove_vuetify_loader_and_execute(callable):
-    vuetify_loader_removed = False
-    if component_library_used == 'vuetify':
-        # Remove vuetify-loader, so we can load a Jupyter compatible version of Vuetify. Ideally
-        # this should be configured with vue-cli, but I've not yet figured out how to do that.
-        try:
-            execute(vue_project_dir, 'yarn remove vuetify-loader')
-            vuetify_loader_removed = True
-        except CalledProcessError:
-            pass
-    try:
-        callable()
-    finally:
-        # Undo the removal of vuetify-loader, so yarn serve works again.
-        if vuetify_loader_removed:
-            execute(vue_project_dir, 'yarn add vuetify-loader@^1.2.2 --dev')
-
-
-def _build_vue_project():
-    if not directory_exists(f'{vue_project_dir}/node_modules'):
-        execute(vue_project_dir, 'yarn install')
-
-    execute(
-        vue_project_dir,
-        f'npx vue-cli-service build --target lib --name component src/{settings["entry"]}')
 
 
 def install_extension_dependencies():
